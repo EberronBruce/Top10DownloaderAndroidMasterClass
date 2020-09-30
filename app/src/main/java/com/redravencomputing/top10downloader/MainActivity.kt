b9bfc6e6c6fc371b1f95e14046e61d6201c792e4
Bruce <bruce@redravencomputing.com>
2020-09-29 22:58:32 -0600

    Testing AsyncTask

--- app/src/main/java/com/redravencomputing/top10downloader/MainActivity.kt
+++ app/src/main/java/com/redravencomputing/top10downloader/MainActivity.kt
@@ -11,18 +11,26 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         Log.d(TAG, "onCreate called")
+        val downloadData = DownloadData()
+        downloadData.execute("URL goes here")
+        Log.d(TAG, "onCreate done")
     }
 
-    private inner class DownloadData : AsyncTask<String, Void, String>() {
-        private val TAG = "DownloadData"
+    companion object {
+        private class DownloadData : AsyncTask<String, Void, String>() {
+            private val TAG = "DownloadData"
 
-        override fun doInBackground(vararg params: String?): String {
-            TODO("Not yet implemented")
-        }
+            override fun doInBackground(vararg params: String?): String {
+                Log.d(TAG, "doInBackground starts with ${params[0]}")
+                return "doInBackground completed"
+            }
 
-        override fun onPostExecute(result: String?) {
-            super.onPostExecute(result)
+            override fun onPostExecute(result: String?) {
+                super.onPostExecute(result)
+                Log.d(TAG, "onPostExecute parameter is $result")
+            }
         }
     }
 }
